e5315b89cc1c654f6aee8be49c7025f805c5ad8a
Eric Sun <ericmsun@gmail.com>
2022-04-07 16:14:45 -0400

    saving welcome message

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +18,12 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("🎉 Hello There 🎉 ")
     }
+});
+
+const welcomeChannelId = "961716174037606450";
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
 });
 
 client.login(process.env.TOKEN)
